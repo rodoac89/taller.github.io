d1360779973bab8356f5e64738f888de582e83f3
Rodolfo <rodo.aravena@outlook.com>
2026-04-01 20:26:59 -0300

    publish

--- githubpages-angular/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ChileWeather } from './chile-weather/chile-weather';
-
-export const routes: Routes = [
-  { path: '', component: ChileWeather },
-];
